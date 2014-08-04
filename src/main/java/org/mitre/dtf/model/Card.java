190a4eeb79c23610882e08e1bcbfe54ec5cfc40a
William Kim <wikkim@gmail.com>
2014-08-04 15:14:13 -0400

    fleshed out Card and CardRepository, and added some unit testing

--- src/main/java/org/mitre/dtf/model/Card.java
+++ src/main/java/org/mitre/dtf/model/Card.java
@@ -83,4 +83,39 @@ public class Card {
 	public String toString() {
 		return "Card [Title=" + title + ", Description=" + description + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
 }
--- src/main/java/org/mitre/dtf/repository/JpaCardRepository.java
+++ src/main/java/org/mitre/dtf/repository/JpaCardRepository.java
@@ -1,5 +1,7 @@
 package org.mitre.dtf.repository;
 
+import static org.mitre.util.jpa.JpaUtil.saveOrUpdate;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -11,6 +13,12 @@ import org.mitre.dtf.model.Card;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Repository layer for translating between POJOs and database objects.
+ * 
+ * @author wkim
+ *
+ */
 @Repository("jpaCardRepository")
 public class JpaCardRepository {
 	
@@ -23,5 +31,26 @@ public class JpaCardRepository {
 		TypedQuery<Card> query = em.createNamedQuery("Card.findAll", Card.class);
 
 		return new LinkedHashSet<Card>(query.getResultList());
+	}
+	
+	@Transactional
+	public Card getById(long id) {
+		return em.find(Card.class, id);
+	}
+	
+	@Transactional
+	public void remove(Card card) {
+		Card found = em.find(Card.class, card.getId());
+
+		if (found != null) {
+			em.remove(card);
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	@Transactional
+	public Card save(Card card) {
+		return saveOrUpdate(card.getId(), em, card);
 	}
 }
